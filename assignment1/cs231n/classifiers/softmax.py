Made a mistake in the second part of the gradient update by forgetting to update for all j (I had omitted the y/correct class). Also forgot the numerical instability trick.

assignment1/cs231n/classifiers/softmax.py
@@ -34,18 +34,25 @@ def softmax_loss_naive(W, X, y, reg):
 
   for i in xrange(num_train):
     scores = X[i].dot(W)
+
+    # Normalization trick to resolve numerical instability
+    # when dealing with the large exponential terms.
+    scores -= np.max(scores)
+
+    # Cache some terms that are used repeatedly.
     exp_scores = np.exp(scores)
     sum_exp_scores = np.sum(exp_scores)
     correct_class_score = scores[y[i]]
-    exp_sum = 0.0
-    for j in xrange(num_classes):
-      if j == y[i]:
-        loss -= correct_class_score
-        dW[:,j] -= X[i,:].T
-      else:
-        dW[:,j] += ((X[i,:].T * exp_scores[j]) / sum_exp_scores)
-      # exp_sum += np.exp(scores[j])
+    
+    # Update the loss
+    loss -= correct_class_score
     loss += np.log(sum_exp_scores)
+
+    # Update the gradient
+    dW[:,y[i]] -= X[i,:].T
+    for j in xrange(num_classes):
+      dW[:,j] += ((X[i,:].T * exp_scores[j]) / sum_exp_scores)
+
   
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -82,6 +89,7 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
+
   pass
   #############################################################################
   #                          END OF YOUR CODE                                 #