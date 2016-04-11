b75a71272facd8323122151f57f2aa1137aa6412
Barry Ridge <barry.ridge@gmail.com>
2016-04-11 12:51:43 +0200

    Finished rmsprop + adam implementations.

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -106,6 +106,10 @@ def rmsprop(x, dx, config=None):
   # in the next_x variable. Don't forget to update cache value stored in      #  
   # config['cache'].                                                          #
   #############################################################################
+
+  config['cache'] = config['decay_rate'] * config['cache'] + (1 - config['decay_rate']) * dx**2
+  next_x = x - config['learning_rate'] * dx / (np.sqrt(config['cache']) + config['epsilon'])
+
   pass
   #############################################################################
   #                             END OF YOUR CODE                              #
@@ -143,6 +147,11 @@ def adam(x, dx, config=None):
   # the next_x variable. Don't forget to update the m, v, and t variables     #
   # stored in config.                                                         #
   #############################################################################
+
+  config['m'] = config['beta1'] * config['m'] + (1 - config['beta1']) * dx
+  config['v'] = config['beta2'] * config['v'] + (1 - config['beta2']) * (dx**2)
+  next_x = x - config['learning_rate'] * config['m'] / (np.sqrt(config['v']) + config['epsilon'])
+
   pass
   #############################################################################
   #                             END OF YOUR CODE                              #
